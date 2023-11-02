936f428a9091f19620b26b2571a503158a04124d
legipaul <84641492+legipaul@users.noreply.github.com>
2023-11-02 09:00:28 +0530

    Add files via upload

---
+++ Pages/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthWest.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/WishLists.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthWest.Pages
+{
+    public class WishListsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
